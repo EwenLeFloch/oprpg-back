e30038fb1aa0e8cb6043d99069b10a4aab2ccfd7
EwenLeFloch <ewenlelf@gmail.com>
2026-05-15 21:45:37 +0200

    Création entité Utilisateur

---
+++ src/main/java/com/onepiecerpg/api/entity/Utilisateur.java
@@ -0,0 +1,27 @@
+package com.onepiecerpg.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "utilisateur")
+@Getter
+@Setter
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String mail;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "USER";
+}
